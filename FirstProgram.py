a7d3b51fabdff28a43a7092c769987401b9b5f04
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-25 00:58:54 +0530

    add

---
+++ FirstProgram.py
@@ -0,0 +1,34 @@
+# print("hello world") 
+# print(28+20)
+# name="Hello world"
+# price=20;
+# age=22;
+
+# print("name is:", name,"age is:",age ,"Price is :", price)
+
+# arithmatic operators
+""" a = 4;
+b = 2;
+# sum=a+b
+print(a + b);
+print(a - b);
+print(a * b);
+print(a % b);
+print(a / b) """
+# print("hello world")
+
+# relational operators
+# a=10;
+# b=11;
+# print(a==b)
+# print(a!=b)
+# print(a>=b);
+# print(a<=b);
+
+# Assignment operators
+num=10
+num=num+10
+print(num)
+
+
+
