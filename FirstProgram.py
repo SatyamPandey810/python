0eca1d31432498d8a54a6fd89b2db55c6f99f1f4
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-25 01:27:31 +0530

    name

--- FirstProgram.py
+++ FirstProgram.py
@@ -26,9 +26,42 @@ print(a / b) """
 # print(a<=b);
 
 # Assignment operators
-num=10
-num=num+10
-print(num)
+# num=10
+# num**=2
+# print(num)
+
+# Logical operators
+# print(not True)
+# print(not False)
+
+# a=True;
+# b=False;
+
+# print(a and b)
+# print(a or b)
+# print(a==b or a>b)
+
+# print(not(a>b))
+#  a=10
+#  b=102
+ 
+# type conversion
+# a=int("2");
+# b=4.34
+# sum=a+b
+# print(type (a))
+# print(sum)
+
+# string conversion
+# a = 2.3;
+# a = str(a)
+# print(type(a))
+# print(a)
+
+# input
+# name =float(input("enter your name"))
+# print("wellcome",type(name),name)
+
 
 
 
