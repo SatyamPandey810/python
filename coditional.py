d9083d8b0ccd9174f9a1f716d9c92ffa02558eac
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-26 00:51:32 +0530

    add

---
+++ coditional.py
@@ -0,0 +1,72 @@
+#if
+# age =11
+# if(age>=18):
+#     print("You are aligble for voting")
+# else:
+#     print("Your can't voting")
+
+# elif
+# light="yellow"
+# if(light == "red"):
+#     print("Stop your vehicle")
+# elif(light == "green"):
+#     print("Go now")
+# elif(light == "yellow"):
+#     print("much you stop can")
+
+# marks=int(input("Enter marks"));
+# if(marks>=80):
+#     print("Grade A")
+# elif(marks>=60):
+#     print("Grade B")
+# elif(marks>=40):
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+# age=10;
+# if(age>=18):
+#     print("Drive on")
+# else:
+#     print("Don't drive")
+
+# practice questions
+# num=int(input("Enter your number :"))
+# if(num%2==0):
+#         print("number is even");
+# elif(num%2!=0):
+#         print("Number is odd")
+        
+# num =int(input("enter first number"));       
+# num1=int(input("enter second number"));
+# num2=int(input("enter third number"))
+
+# if(num>=num1 and num>=num2):
+#     print("first",num ,"is greater than all")
+# elif(num1>=num and num1>=num2):
+#     print("second", num1, "is grater than all")
+# else:
+#     print("third", num2,"is greate than all")
+
+# a=int(input("Enter your first number :"))
+# b=int(input("Enter second number :"))
+# c=int(input("Enter your third number :"))
+# d=int(input("Enter your fourth number :"))
+
+# if(a>=b and a>=c and a>=d):
+#     print("first number is :", a,"is greate than all")
+# elif(b>=a and b>=c and b>=d):
+#     print("Second number is :" ,b ,"is greater than all")
+
+# elif(c>=a and c>=b and c>=d):
+#     print("Third number is :",c,"is greater than all")
+# else:
+#     print("Fourth number: ",d,"is greater than all")
+
+
+number=int(input("Enter your number"))
+
+if(number%7==0):
+ print("Number is multiply 7")
+else:
+    print("Number is not multiply 7 try again")
