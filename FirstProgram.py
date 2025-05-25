638e323a8dc744a43762d5c0bf42fc722251cbd7
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-25 23:56:10 +0530

    add

--- FirstProgram.py
+++ FirstProgram.py
@@ -62,6 +62,64 @@ print(a / b) """
 # name =float(input("enter your name"))
 # print("wellcome",type(name),name)
 
+# firstNumber=int(input("enter first number"))
+# secondNumber=int(input("enter second number"))
+
+# print("your number total is",firstNumber**2)
+
+# float number to average
+# f=float(input("Enter your number"));
+# g=float(input("Enter your number"));
+
+# print("Avg",(f+g/2))
+
+# true and false
+# a=int(input("Enter first number"))
+# b=int(input("Enter your second number"))
+
+# print(a>=b)
+
+# string
+# str="Hello world";
+# str1=str[0];
+# print(str1);
+
+# str="Hello world"
+# str1=(str[5:len(str)])
+# print(str1)
+
+# str="Hello world"
+# print(str[-5:])
+
+# str="Hello world";
+# print(str.endswith("world"));
+
+#Capitalize
+# str="hello world";
+# print(str.capitalize());
+# str=str.capitalize()
+# print(str);
+
+# replace
+# str="hello world";
+# print(str.replace("hello" ,"kya hal"))
+
+#find
+# str="Hello world";
+# print(str.find("d"))
+
+# count
+# str="hello world"
+# print(str.count("h"))
+# print(str.isalpha("o"))
 
 
+# practice
+# 1.
+# str=input("Enter your name :");
+# print("Your name length is:",len(str))
+
+# 2.
+# str="$jkal$$$$$";
+# print(str.count("$"));
 
