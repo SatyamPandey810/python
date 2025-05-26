dfe7dbc1b40d55180c9d4ec6fe3b282a1d0be24f
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-27 00:53:50 +0530

    add

---
+++ DictionaryAndSet.py
@@ -0,0 +1,10 @@
+bioData={
+    "name":"Jhon due",
+    "age":(1,3,4,6),
+    "number":123456,
+    "isActive":True,
+    "hobby":["cricket","football"]        
+}
+bioData["name"]="name"
+print(bioData["name"] );
+print(type(bioData))
---
+++ ListAndTuple.py
@@ -0,0 +1,91 @@
+# marks=[10,20,30];
+# print(marks[0]);
+# print(type(marks));
+# print(len(marks));
+# list are imutable
+# student=["satyam",25 ,"python", 4.5];
+# student[1]="rahul"
+# print(student)
+
+#Sort
+# lis=[10,20,30,40];
+# lis=["a","b","c","d"]
+# lis.append(4)
+# lis.sort(reverse=True)
+# lis.sort()
+# print(lis)
+# print(lis[-3:-1])
+
+# reverse
+# list=[1,2,3,5]
+# list.reverse()
+# print(list)
+
+#insert
+# list=[1,2,3,4]
+# list.insert(1,"hello world")
+# print(list)
+
+#remove
+# list=[1,2,3,4];
+# list.remove(1)
+# print(list)
+
+#pop
+# list=[1,2,3,4]
+# list.pop()
+
+#clear
+# list.clear()
+# list.copy()
+# print(list.copy())
+
+#-------------===------===----tuple
+
+# tupl=(1,2,3,4);
+# print(type(tupl));
+# print(tupl[0])
+# print(tupl[1])
+
+# tuple=(1,2,3,4,1,1)
+# tuple.index(2)
+#index
+#print(tuple.index(4))
+#count
+# print(tuple.count(1))
+
+# practice-1
+# list1=(input("Enter your first movie name :"))
+# list2=(input("Enter your second movie name :"))
+# list3=(input("Enter your third movie name :"))
+
+# final="Your fav movie is :-",[list1,list2,list3]
+# print(final)
+
+# practice-2
+# list1=[1,2,3];
+# copy_list=list1.copy()
+# copy_list.reverse()
+
+# if(copy_list==list1):
+#     print("polindrome")
+# else:
+#     print("Not polindrome")
+
+# student=("A","A","B","C","C" ,"B","fail","B","A")
+# a=student.count("A")
+# b=student.count("B")
+# c=student.count("C")
+# d=student.count("fail")
+
+# if(a):
+#     print(a,"Student are find grade A")
+# if(b):
+#     print(b,"Student is find grade B")
+# if(c):
+#     print(c,"Student is find grade B")
+# if(d):
+#     print(d,"Student is fail")
+
+
+
