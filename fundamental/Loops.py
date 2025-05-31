71f956f85e8d4f7d6bb5219cdcdc5387362a2ccd
satyam81 <satyampandeyrewa810@gmail.com>
2025-05-31 23:27:10 +0530

    add

---
+++ fundamental/Loops.py
@@ -0,0 +1,79 @@
+# while loop
+
+# count=0
+# while count<=5:
+#     print(count)
+#     count+=1
+    
+        
+#practice
+# n=1;
+# while n<=100:    
+#     print(n)
+#     n+=1
+    
+    
+# n=100;
+# while n>=0:
+#     print(n)
+#     n-=1
+
+# user=int(input("Enter Number:"))
+# n=1;
+# while n<=10:
+#     print(user*n)
+#     n+=1
+
+# nums=[1,2,9,16,25,36,49,64,81,100];
+# nunm1=["ram","shyam","ghanshyam"];
+# indx=0;
+# while indx<len(nunm1):
+#     print(nunm1[indx])
+#     indx +=1
+ 
+# lis=[1,2,3,4,5,6];  
+# inx=0;
+# while inx < len(lis):
+#     print(lis[inx])
+#     inx+=1
+
+
+# Q-2
+# tupple=(1,2,3,4,5,6)
+# x=10;
+# i=0;
+# while i < len(tupple):
+#     if(tupple[i] == x):
+#         print("number is",i )
+#     else:
+#         print("not found")    
+#     i += 1
+
+# Break keyword
+# i=1
+# while i<=5:
+#     print(i) 
+#     if(i==3):
+#         break
+#     i+=1
+#     print("loops is stopble")
+
+# continue keyword
+# i=0;
+# while i<=5:
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# odd/even
+# i=1
+# while i<=100:
+#     if(i%2==0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# for loop--------------------------------
