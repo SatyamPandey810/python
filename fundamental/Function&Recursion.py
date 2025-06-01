7bf3d31fa617f7f86816da859a6abcdc16616f43
satyam81 <satyampandeyrewa810@gmail.com>
2025-06-02 01:56:26 +0530

    add

--- fundamental/Function&Recursion.py
+++ fundamental/Function&Recursion.py
@@ -1,3 +1,4 @@
+## functions---------------------
 # def func1(a,b):
 #     sum=a+b;
 #     print(sum)
@@ -9,6 +10,103 @@
 # sum=calu(2,3)
 # print(sum)
     
-def print_hello() :
-    print("Hello world")  
-print_hello()    
+# def print_hello(a):
+#    return a 
+# sum=print_hello("hello world")    
+# print(sum)
+
+# def calculate(a,b,c):
+#     sum=a+b+c;
+#     avg=sum/3
+#     return avg
+
+# cal=calculate(10,50,30)
+# print(cal)
+    
+# Praactice    
+# num=[1,2,3,4,5,6];
+# city=["Delhi","Noida","gudhgao","america"]
+
+# def lenFun(list,country):
+#     print(len(list))
+#     print(len(country))
+    
+# lenFun(num)
+# lenFun(city)    
+ 
+#Q.2    
+# city=["Delhi","Noida","gudhgao","america"]
+# def print_city():
+#     print(city[0],end=" \n")
+#     print(city[2],end=" ")
+    
+# print_city()    
+
+# def print_list():
+#     for item in city:
+#         print(item,end=" ")
+# print_list()        
+        
+# Q.3
+# n=5;
+# fact=1
+# for i in range(1,n+1):
+#     fact *=i
+# print(fact)   
+
+# def factorial(number):
+#     no=1;
+#     for item in range(1,number+1):
+#         no*=item
+#     print(no)   
+# factorial(4)
+
+#Q.3
+# def conversion(rs):
+#     print(rs*83)
+# conversion(2)    
+
+#Q.4
+# user=int(input("Enter number :"))
+# def odd_even(user):
+#     if(user%2==0):
+#         print("number is even");
+#     elif(user%2 != 0):
+#         print("Number is odd") ;   
+
+# odd_even(user)
+
+## recursion========--------=-=-=-=-=-=-=-=-====-==-=-=                
+# def recu_fun(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     recu_fun(n-1)
+# recu_fun(20)    
+ 
+    
+# def recur_fun(n):
+#     if(n==0):
+#         return
+#     print(n)    
+#     recur_fun(n-1)
+# recur_fun(4)    
+
+# factorial with recursion
+# def func_1(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return func_1(n-1)*n
+# print(func_1(5))
+        
+# Practice
+# Q.1
+# def func_1(number):
+#     if(number==0):
+#         return 0
+#     return func_1(number-1)+number
+# print(func_1(5))
+
+# Q.2
+
+
