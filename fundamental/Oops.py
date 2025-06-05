ee96ffc6fbdf84501f73e023b49444aa3f85ddce
satyam81 <satyampandeyrewa810@gmail.com>
2025-06-05 21:49:58 +0530

    add

---
+++ fundamental/Oops.py
@@ -0,0 +1,290 @@
+# class Car:
+#     name="TATA"
+#     model="2020"
+
+# si=Car();    
+# print(si.name);
+
+# constructor ---------
+    # name="Maruti"
+    # color="Red"
+    
+# class Car:
+#     # Default constructor
+#     def _init__(self):
+#         pass
+       
+#      # parameterize constructor  
+#     def __init__(self,name,color):
+#         # print(fullName)
+#         self.name=name;
+#         self.color=color;
+#         print("Adding a new student in database")
+
+# car=Car("Maruti","Red")    
+# print(car.name,car.color)
+# car2=Car("TATA","Blue")
+# print(car2.name,car2.color)
+
+# class Student:
+#     college="ABC";
+#     name="john due"  #class attribute
+#     def __init__(self,name,marks):
+#         self.name=name;  # object attribute 
+#         self.marks=marks;
+# student=Student("raju",450)       
+
+# print(student.name,student.marks);
+# # print(student.college)
+# print(Student.college)
+# print(student.name)
+
+
+# methods -----------------------
+# class Car:
+#     def __init__(self,name,marks):
+#         self.name=name
+#         self.marks=marks
+        
+#     def hello(self):
+#         print("wellcome student",self.name)
+        
+#     def mark(self):
+#         return self.marks
+    
+    
+# car=Car("raju",30)  
+# car.hello()
+# print(car.mark())
+
+
+#practice
+# class Student:   
+#     def __init__(self,name,marks):
+#         self.name=name;
+#         self.marks=marks    
+#     @staticmethod 
+#     def hello():
+#         print("hello worlds")       
+#     def get_avg(self):
+#          sum=0
+#          for item in self.marks:
+#              sum+=item;
+#              print("hii",self.name,"your avarage is :",sum/3)      
+# student=Student("ram",[98,97,93])        
+# # print(student.name,student.marks) 
+# student.get_avg()    
+# student.hello()  
+# student.hello() 
+
+###### Abstactions----------------
+# class Car():
+#     def __init__(self):
+#         self.acc=False
+#         self.bre=False
+#         self.clutch=False
+#     def car_start__(self):
+#        self.acc=True
+#        self.bre=True
+#        self.clutch=True
+#        print("start the car....")      
+       
+# car=Car()
+# # print(car)
+# car.car_start__()
+
+#Practice
+# class Account():
+#     def __init__(self,bal,acc):
+#         self.acc=acc;
+#         self.bal=bal;
+        
+#     def debit(self,amount):
+#         self.bal-=amount
+#         print("Rs",amount,"was debited")
+#         print("Total balance is :" ,self.get_balance())
+           
+#     def credit(self,amount):
+#         self.bal+=amount
+#         print("Rs",amount,"was created")    
+#         print("Total balance is :" ,self.get_balance())
+                   
+   
+#     def get_balance(self):
+#        return self.bal
+   
+# account=Account(10000,12345)        
+# print(account.bal)
+
+# account.debit(1000)
+# account.credit(200)
+        
+# class Pocket:
+#     def __init__(self,bal,acc):
+#         self.bal=bal;
+#         self.acc=acc;
+        
+#     def debit(self,amount):
+#         self.bal-=amount
+#         print("Rs.",amount, "is debited")    
+#         print("Total balance is",self.total_balance())
+
+#     def credit(self,amount):
+#         self.bal+=amount
+#         print(amount,"is credited")   
+#         print("Total balance is",self.total_balance())
+
+#     def total_balance(self):
+#        return self.bal    
+        
+# pocket=Pocket(8000,1223443)        
+
+# pocket.debit(2000)
+# pocket.credit(1000)
+# pocket.total_balance()
+
+# del keyword==-----==-=-=-=-=-=-=---=-=-=-=-===\
+# class Car:
+#     def __init__(self,name):
+#         self.name=name      
+        
+# s1=Car("ram")      
+# del s1  
+# print(s1.name)
+
+# private property and methods
+# property private---------------------------
+# class Bank:
+#     def __init__(self,accno,pasbok):
+        
+#         self.accno=accno
+#         self.__pasbok=pasbok
+        
+#     def resetPassword(self):
+#         print(self.__pasbok)        
+        
+# bank=Bank("12345","abcde")        
+
+# bank.resetPassword()
+# print(bank.accno)
+# print(bank.pasbok)
+  
+# method private=================
+# class Bank:
+#     __name="HDFC BANK"    
+#     def __hello(self ):
+#         print("Hello world")
+#     def wellcome(self):
+#         self.__hello()      
+# bank=Bank()    
+# # print(bank.name)
+# bank.wellcome()
+
+#  inheritance======================================
+# single inheritance
+# class Car:
+#     color="White"
+#     @staticmethod
+#     def start():
+#         print("start car....")
+#     @staticmethod
+#     def stop():
+#         print("stop car")    
+
+# class Toyota(Car):        
+#     def __init__(self,name):
+#         self.name=name
+        
+# car1=Toyota("fourtuner")        
+# print(car1.name)
+# print(car1.start())
+# print(car1.color)
+
+# multi-level inheritance
+# class Car:
+#     color="White"
+#     @staticmethod
+#     def start():
+#         print("start car....")
+#     @staticmethod
+#     def stop():
+#         print("stop car")    
+
+# class Toyota(Car):        
+#     def __init__(self,brand):
+#         self.brand=brand
+        
+# class Fortuner(Toyota):
+#     def __init__(self,type):
+#         self.type=type       
+        
+# car1=Toyota("toyota hi brand hai")    
+# print(car1.color)
+
+# car2 =Fortuner("diesel")
+# car2.start()
+# print(car2.type)
+
+#Multiple inheritance
+# class Parent1:
+#     varA="hello i am parent-1"
+# class Parent2:
+#     varB="hello i am parent-2"    
+# class Child(Parent1,Parent2):
+#     varChild=("Hello all i am child")    
+    
+# child=Child()    
+# print(child.varA)
+# print(child.varB)
+# print(child.varChild)
+
+#super keyword
+# class A:
+#     def __init__(self):
+#         print("hello i am parent constructor")
+
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#         print("hello i am child constructor")
+        
+# b=B()        
+# print(b)
+ 
+# class Car:
+#     def __init__(self,type):
+#         self.type=type
+#     @staticmethod
+#     def start():
+#         print("car startd..")
+# class Tyota(Car):
+#     def __init__(self,type,name):
+#        super().__init__(type)
+#        self.name=name 
+#        super().start()
+# c1=Tyota("tata","petrol")       
+# print(c1.name)
+# print(c1.type)
+# c1.start()
+
+# class Person:
+#     name="deepak"
+#     def nameChagne(self,name):
+#         Person.name=name
+        
+# person=Person()        
+# person.nameChagne("rahul")
+# print(Person.name)
+# print(person.name)
+        
+        
+        
+
+
+
+ 
+
+
+
+
+
