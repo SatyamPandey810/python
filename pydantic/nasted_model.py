6158a257b12bc1124e25e7bce3095dc439205763
satyam81 <satyampandeyrewa810@gmail.com>
2025-08-25 00:12:19 +0530

    add

--- pydantic/nasted_model.py
+++ pydantic/nasted_model.py
@@ -1,9 +1,36 @@
 from pydantic import BaseModel
 
-
+class Address(BaseModel):
+    city:str
+    state:str
+    pin:str
 
 class Patient(BaseModel):
     name:str
     gender:str
     age:int
-    address:
+    address:Address
+    
+    
+address_dict={'city':'noida','state':'UP','pin':'23232'}    
+address1=Address(**address_dict)
+patients_dict={'name':'john','gender':'male','age':30,'address':address1}
+patient1=Patient(**patients_dict)
+
+
+print(patient1)
+print(patient1.name)
+print(patient1.address.pin)
+
+
+
+
+temp=patient1.model_dump(include=['name'])   # exclude
+# temp=patient1.model_dump_json()
+
+
+print(temp)
+# print(type(temp))
+# print(type(temp))
+
+
